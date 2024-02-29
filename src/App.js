7e710f45b43e8558da15e9fe3e2a92d4282c5053
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-02-29 19:06:16 +0100

    Non

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import EventsPage, { loader as eventsLoader } from "./pages/EventsPage";
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from "./pages/EventDetailPage";
-import NewEventPage from "./pages/NewEventPage";
+import NewEventPage, { action as newEventAction } from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
 import RootLayout from "./pages/Root";
 import EventsRootLayout from "./pages/EventsRoot";
@@ -70,7 +70,7 @@ const router = createBrowserRouter([
           {
             path: "new",
             element: <NewEventPage />,
-            // action:
+            action: newEventAction,
           },
         ],
       },
--- src/pages/NewEventPage.js
+++ src/pages/NewEventPage.js
@@ -1,3 +1,4 @@
+import { json, redirect } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 export default function NewEventPage() {
@@ -5,7 +6,7 @@ export default function NewEventPage() {
 }
 
 export async function action({ request, params }) {
-  const data = request.formData();
+  const data = await request.formData();
 
   const eventData = {
     title: data.get("title"),
@@ -14,8 +15,21 @@ export async function action({ request, params }) {
     description: data.get("description"),
   };
 
-  fetch("http://localhost:8080/events", {
+  const response = fetch("http://localhost:8080/events", {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(eventData),
   });
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not save event" },
+      {
+        status: 500,
+      }
+    );
+  }
+  return redirect("/events");
 }
